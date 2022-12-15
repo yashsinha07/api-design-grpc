a94bc8e356c921ea624f36ad52f53f1edcfd1688
yashsinha <yashsinha7696@gmail.com>
2022-12-14 19:01:21 -0500

    TESTS -> Mock using patch, unit tests implemented.

---
+++ client/test.py
@@ -0,0 +1,26 @@
+import unittest
+from unittest.mock import patch
+
+import client.inventory_client
+from inventory_client import InventoryClient
+
+
+class GetBookTitlesTest(unittest.TestCase):
+
+    # MockTest
+    @patch('client.inventory_client.InventoryClient')
+    def test(self, Client):
+        client.inventory_client.InventoryClient('localhost:blahblah')
+        assert Client is client.inventory_client.InventoryClient
+        assert Client.getBookTitle(["1001", "1002"])
+
+    # Unit tests
+    def test_normal_case(self):
+        inventoryClient = InventoryClient(serverAddress='localhost:50051')
+        self.assertEqual(inventoryClient.getBookTitle(["1001", "1002"]),
+                         ['Harry Potter', 'Fault In Our Stars'],
+                         "Incorrect response.")
+
+    def test_negative_case(self):
+        inventoryClient = InventoryClient(serverAddress='localhost:50051')
+        self.assertEqual(inventoryClient.getBookTitle(["1003"]), [''], "ISBN not found!")
